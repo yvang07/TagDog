4c96bbd5041bfc69c8ea0e23b876bd9dfbea4774
Yang Vang <vang_boi89@hotmail.com>
2012-03-24 13:58:37 -0700

    added menu.php

--- TagDog_CI/application/config/config.php
+++ TagDog_CI/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://cloudedbox.com/CI/TagDog/';
+$config['base_url']	= 'http://localhost/TagDog/TagDog_CI/';
 
 /*
 |--------------------------------------------------------------------------
