e80a3cb667c8f2174953ba67dd05993747b37cd9
Francisco Perez <franceiskool@gmail.com>
2012-03-24 14:09:17 -0700

    Included <a> CSS features

--- TagDog_CI/application/controllers/menu.php
+++ TagDog_CI/application/controllers/menu.php
@@ -6,8 +6,8 @@ class Menu extends CI_Controller {
 
 	function index() {
 
-		$data['main_content'] = 'view_menu';
-		$this->load->view('includes/template', $data);
+		//$data['main_content'] = 'view_menu';
+		$this->load->view("view_menu");
 	}
 
 
