01d5317485f763a49f797b21db2cf3af2243103c
Francisco Perez <franceiskool@gmail.com>
2012-04-16 15:25:36 -0700

    changed the name of the text fields to c_line1, c_line2...etc.  --Francisco Perez

--- TagDog/application/views/view_create.php
+++ TagDog/application/views/view_create.php
@@ -3,23 +3,23 @@
             <small>Type and you shall receive.</small>
             <div id="create_container">
 		<div class="row-fluid show-grid">
-		    <div class="span6 offset1" id="create_left">
+		    <div class="span5 offset1" id="create_left">
 			    <form onsubmit="createTAG(); return false;">
-				<input id="fname_create" placeholder="Name" type="text" />
-				<input id="lname_create" placeholder="Address" type="text" />
-				<input id="user_text" placeholder="Phone"type="text" />
-				<input id="user_text" placeholder="Item" type="text" />
+				<input id="c_line1" placeholder="Name" type="text" />
+				<input id="c_line2" placeholder="Address" type="text" />
+				<input id="c_line3" placeholder="Phone"type="text" />
+				<input id="c_line4" placeholder="Item" type="text" />
 				<input type="submit" value="create" />
 			    </form>
 		    </div>
-		    <div class="span6" id="create_right">
+		    <div class="span5 offset1" id="create_right">
 			    <img id="QR_code" alt=" No QR code :(">
 			    <script>
 				function createTAG()
 				{
 				    var URL="https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl="
-				    var fname=document.getElementById("fname_create").value;
-				    var lname=document.getElementById("lname_create").value;
+				    var fname=document.getElementById("c_line1").value;
+				    var lname=document.getElementById("c_line2").value;
 				    var TAG=document.getElementById("QR_code");
 				    TAG.src=URL+fname+lname;
 				    jQuery("button#share_button_preview").html("<img src='"+URL+fname+lname+"' />");
