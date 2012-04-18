a150482615371f46e1fe757cb05164bb4452f5bc
Francisco Perez <franceiskool@gmail.com>
2012-04-18 15:30:22 -0700

    all lines now create the Tag. --Francisco P.

--- TagDog/application/views/view_create.php
+++ TagDog/application/views/view_create.php
@@ -1,41 +1,32 @@
             <h4 id="create">CREATE<small class="span1"><i class="icon-plus"></i></small></h4>
             <p>
             <small>Type and you shall receive.</small>
-            <div id="create_container">
 		<div class="row-fluid show-grid">
-		    <div class="span5 offset1" id="create_left">
+		    <div class="span6" id="create_left">
 			    <form onsubmit="createTAG(); return false;">
 				<input id="c_line1" placeholder="Name" type="text" />
 				<input id="c_line2" placeholder="Address" type="text" />
 				<input id="c_line3" placeholder="Phone"type="text" />
-				<input id="c_line4" placeholder="Item" type="text" />
-				<input type="submit" value="create" />
+				<input id="c_line4" placeholder="Item" type="text" /></br>
+				<button class="btn btn-info btn-large" type="submit" value="create" />Create</button>
 			    </form>
 		    </div>
-		    <div class="span5 offset1" id="create_right">
-			    <img id="QR_code" alt=" No QR code :(">
+		    <div class="span6" id="create_right">
+			    <button class="btn" />Code Will Appear Here</br><img id="QR_code" src=""/></button>
 			    <script>
 				function createTAG()
 				{
 				    var URL="https://chart.googleapis.com/chart?cht=qr&chs=300x300&chl="
-				    var fname=document.getElementById("c_line1").value;
-				    var lname=document.getElementById("c_line2").value;
+				    var line1=document.getElementById("c_line1").value;
+				    var line2=document.getElementById("c_line2").value;
+				    var line3=document.getElementById("c_line3").value;
+				    var line4=document.getElementById("c_line4").value;
 				    var TAG=document.getElementById("QR_code");
 				    TAG.src=URL+fname+lname;
-				    jQuery("button#share_button_preview").html("<img src='"+URL+fname+lname+"' />");
+				    jQuery("button#share_button_preview").html("<img src='"+URL+line1+line2+line3+line4"' />");
 				}
-			    </script>
+			   </script>
 		    </div>
 		</div>
-            </div>
-	    <!--</br>
-	    </br>
-	    </br>
-	    </br>
-	    </br>
-	    </br>
-	    </br>
-	    </br>
-	    </br>
-	    </br> -->
 	    </p>
+
