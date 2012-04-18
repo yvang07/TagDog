4f178c9dbcfab7fa3eff378c4cee68fd317f80c5
Cameron <cameronbriar@mail.fresnostate.edu>
2012-04-18 15:24:34 -0700

    Accidentally removed controller

--- TagDog/application/views/includes/header.php
+++ TagDog/application/views/includes/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>[TAGDOG]</title>
-    <meta name="viewport" content="width=900px, initial-scale=0.4">
+    <meta name="viewport" content="width=600px, initial-scale=0.4">
     <meta name="description" content="">
     <meta name="author" content="">
 
