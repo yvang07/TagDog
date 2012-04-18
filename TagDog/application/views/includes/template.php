99f64acf124a1099bdf88db1a99ccf7107f567de
Yang Vang <vang_boi89@hotmail.com>
2012-04-18 11:54:36 -0700

    added cameron

---
+++ TagDog/application/views/includes/template.php
@@ -0,0 +1,19 @@
+<?php $this->load->view('includes/header'); ?>
+
+<?php $this->load->view('includes/navbar'); ?>
+
+<?php $this->load->view($main_content); 
+
+	$this->load->view('view_create');
+	$this->load->view('view_identify');
+	$this->load->view('view_browse');
+	$this->load->view('view_share');
+	$this->load->view('view_toggles');
+	
+	//Modal
+	$this->load->view('view_modal_about');
+	$this->load->view('view_modal_tags');
+	$this->load->view('view_modal_contact');
+?>
+
+<?php $this->load->view('includes/footer'); ?>
---
+++ TagDog/application/views/view_identify.php
@@ -0,0 +1,21 @@
+<h4 id="identify">IDENTIFY<small class="span1"><i class="icon-qrcode"></i></small></h4>
+<p>
+        <small>Tell all of your friends!</small>
+	
+	<div id="share_container">
+	<div class="row-fluid show-grid">
+	<div class="span4" id="share_left">
+		<div id="share_icon"></div>
+		<div id="share_icon"></div>
+	</div>
+	<div class="span4" id="share_center">
+		<div class="share_previews"></div>
+		<button class="btn" id="identify_drop">DROP QR CODE HERE</button>
+	</div>
+	<div class="span4" id="share_right">
+		<div id="share_icon"></div>
+		<div id="share_icon"></div>
+	</div>
+	</div>
+	</div>
+</p>
